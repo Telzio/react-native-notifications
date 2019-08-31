fc81b882708f0cb33041acf3a6d3a215ddcb21cd
Jonas Swiatek <jonas@telzio.com>
2019-08-31 11:39:46 -0700

    Include rnn_hasRNHost in the args sent to the headless task processor. This is used to determine if the app was in a dead or background state upon receiving the message

--- android/app/src/main/java/com/wix/reactnativenotifications/core/notification/HeadlessTaskDispatcher.java
+++ android/app/src/main/java/com/wix/reactnativenotifications/core/notification/HeadlessTaskDispatcher.java
@@ -8,18 +8,28 @@ import com.facebook.react.HeadlessJsTaskService;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+import com.facebook.react.ReactNativeHost;
 
 import javax.annotation.Nullable;
+
+import static com.wix.reactnativenotifications.Defs.LOGTAG;
 
 public class HeadlessTaskDispatcher extends HeadlessJsTaskService {
 
   @Override
   protected @Nullable HeadlessJsTaskConfig getTaskConfig(Intent intent) {
+
+    ReactNativeHost host = getReactNativeHost();
+    Log.d(LOGTAG, "hasInstance(): " + host.hasInstance());
+
     Bundle extras = intent.getExtras();
+    WritableMap args = Arguments.fromBundle(extras);
+    args.putBoolean("rnn_hasRNHost", host.hasInstance());
+
     if (extras != null) {
       return new HeadlessJsTaskConfig(
           "RNNotificationsDataMessageReceived",
-          Arguments.fromBundle(extras),
+          args,
           30000, // timeout for the task
           true // optional: defines whether or not  the task is allowed in foreground. Default is false
         );
